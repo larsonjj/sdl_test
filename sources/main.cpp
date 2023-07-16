7602f4aa51586d613227d071bcaa9942c25c601c
Jake Larson <jake.j.larson@gmail.com>
2023-07-16 06:10:16 -0500

    removed FMT

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-#include <fmt/core.h>
 #include <SDL.h>
 
 int main(int argc, char* args[]) {
     //Initialize SDL2 with appropriate subsystems.
     SDL_Init(SDL_INIT_VIDEO);
-    fmt::print("Hello, world!\n");
-    std::cout << "Hello, World!" << std::endl;
+    SDL_Log("Hello, world!\n");
     SDL_Quit();
     return 0;
 }
