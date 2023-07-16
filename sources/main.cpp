82fb7e09c9447e373e3b40eae22830da9226b45b
Jake Larson <jake.j.larson@gmail.com>
2023-07-16 13:32:11 -0500

    start getting emscirpten to work

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,10 +1,49 @@
-#include <iostream>
+#include "Window.h"
 #include <SDL.h>
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+void handleInput(bool &shouldQuit) {
+    std::optional<SDL_Event> event;
+    while ((event = Window::pollEvent()))
+        switch (event->type) {
+            case SDL_QUIT:
+                shouldQuit = true;
+                break;
+        }
+}
+
+void updateFrame(bool &shouldQuit) {
+    // Input
+    handleInput(shouldQuit);
+    // Update
+    // Render
+}
 
 int main(int argc, char* args[]) {
-    //Initialize SDL2 with appropriate subsystems.
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Log("Hello, world!\n");
+    try {
+        // Application Start
+        Window window("SDL Test");
+
+        // Application Loop
+#ifdef __EMSCRIPTEN__
+        emscripten_set_main_loop(updateFrame, 0, 1);
+#else
+        bool shouldQuit = false;
+        while (!shouldQuit) {
+            updateFrame(shouldQuit)
+        }
+#endif
+    } catch (const std::exception &exception) {
+        auto errorMessage = exception.what();
+        SDL_LogCritical(SDL_LogCategory::SDL_LOG_CATEGORY_ERROR, "%s", errorMessage);
+        SDL_ShowSimpleMessageBox(SDL_MessageBoxFlags::SDL_MESSAGEBOX_ERROR, "Error Message", errorMessage, nullptr);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Must be called even if all subsystems are shut down
     SDL_Quit();
     return 0;
 }
