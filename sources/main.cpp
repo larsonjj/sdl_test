4fca5c7a65d51f9e0f872d9bc418bb343ded1f90
Jake Larson <jake.j.larson@gmail.com>
2023-07-16 03:03:33 -0500

    Fixed build

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <fmt/core.h>
+#include <SDL.h>
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+int main(int argc, char* args[]) {
+    //Initialize SDL2 with appropriate subsystems.
+    SDL_Init(SDL_INIT_VIDEO);
+    fmt::print("Hello, world!\n");
+    std::cout << "Hello, World!" << std::endl;
+    SDL_Quit();
+    return 0;
 }
